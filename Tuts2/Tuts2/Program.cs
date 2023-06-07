73261ea8548894df22f189f1e7bae7bf87fd02ed
Jerry Barrios <jerrybarrios2001@gmail.com>
2023-06-07 16:28:29 +0800

    Operation Tuts

---
+++ Tuts2/Tuts2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tuts2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnum = 100;
+            int secondnum = 99;
+            String firstname = "jerry";
+            String secondname = "barrios";
+            Console.WriteLine(firstnum % secondnum);
+            Console.WriteLine("my full name is " + firstname + " " + secondname); //nyenmyyenyen
+
+        }
+    }
+}
